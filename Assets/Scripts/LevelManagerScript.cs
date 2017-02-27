fbbf62067ddd623751a86c66b318c31318871370
Amha Tsegaye <amha_ts@hotmail.com>
2017-02-27 00:23:41 -0500

    main menu in progress

---
+++ Assets/Scripts/LevelManagerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManagerScript : MonoBehaviour {
+
+	public Transform mainMenu, optionsMenu;
+
+	public void LoadScene(string name){
+		Application.LoadLevel (name);
+	}
+	public void QuitGame(){
+		Application.Quit ();
+	}
+	public void OptionMenu(bool clicked){
+		if (clicked == true) {
+			optionsMenu.gameObject.SetActive (clicked);
+			mainMenu.gameObject.SetActive (false);
+		} else {
+			optionsMenu.gameObject.SetActive (clicked);
+			mainMenu.gameObject.SetActive (true);
+		}
+	}
+
+
+}
