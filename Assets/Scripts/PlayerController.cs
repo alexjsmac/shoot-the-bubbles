e4387a2c39ab09357fb2887d1192dd032a53781b
Alex MacLean <maclean199@gmail.com>
2017-03-26 13:59:46 -0400

    Adds game over screen

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,15 +7,15 @@ public class PlayerController : MonoBehaviour {
 
 	// Gui component for counting text
 	public Text scoreText;
-
-	// score counter
 	public int score;
 
 	public float health = 100f;
-
 	public float healthRemaining= 0f;
-
 	public Image greenHealthBar;
+
+	public float restartDelay = 5f;         // Time to wait before restarting the level
+	Animator anim;                          // Reference to the animator component.
+	float restartTimer;                     // Timer to count up to restarting the level
 
 	// Use this for initialization
 	public void Start () {
@@ -34,5 +34,28 @@ public class PlayerController : MonoBehaviour {
 
 		healthRemaining -= 10;
 		greenHealthBar.transform.localScale = new Vector3 (healthRemaining/100, 1, 1);
+
+		// If the player has run out of health...
+		if(healthRemaining <= 0)
+		{
+			// ... tell the animator the game is over.
+			anim.SetTrigger ("GameOver");
+
+			// .. increment a timer to count up to restarting.
+			restartTimer += Time.deltaTime;
+
+			// .. if it reaches the restart delay...
+			if(restartTimer >= restartDelay)
+			{
+				// .. then reload the currently loaded level.
+				Application.LoadLevel(Application.loadedLevel);
+			}
+		}
+	}
+
+	void Awake ()
+	{
+		// Set up the reference.
+		anim = GetComponent <Animator> ();
 	}
 }
